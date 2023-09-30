415d93dceb8ad810276f43b83f77043cfba26dff
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-09-30 10:40:15 +0200

    añadiendo un camion y modificando el main

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ public class Main {
     public static void main(String[] args) {
 
 Coche renault=new Coche();
+camion gigante=new camion();
 
 System.out.println("Tu coche tiene lo siguiente: " + renault.ruedas + " ruedas" + renault.puertas +
         "puertas" + renault.motor + " cv");
+
+System.out.println("Tu camion tiene lo siguiente: " + gigante.peso + " toneladas" +
+        gigante.capacidad + " kg" + gigante.potencia + " cv" + gigante.remolques + " remolques");
 
 
 
---
+++ src/camion.java
@@ -0,0 +1,15 @@
+public class camion {
+
+    int peso,capacidad,potencia,remolques;
+
+
+    public camion(){
+
+        peso=9;
+        capacidad=3500;
+        potencia=5000;
+        remolques=2;
+
+
+    }
+}
