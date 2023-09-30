241319cc65b8497698a74ffbcdad70e806d1141d
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-09-30 10:32:51 +0200

    coche virtual

---
+++ src/Coche.java
@@ -0,0 +1,23 @@
+public class Coche {
+
+int ruedas;
+int puertas;
+int motor;
+
+
+public Coche(){
+
+  ruedas=4;
+  puertas=6;
+  motor=1299;
+
+
+}
+
+
+
+
+
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+Coche renault=new Coche();
 
-        int billetes_de_100;
-        int billetes_de_20;
-        int billetes_de_5;
-        int monedas_euro;
-        int euros;
-        System.out.println("Introduce el número de billetes de 100: ");
-        Scanner sc=new Scanner(System.in);
-        billetes_de_100=sc.nextInt();
-        billetes_de_100=billetes_de_100*100;
-        System.out.println("Introduce un número de billetes de 20: ");
-        billetes_de_20=sc.nextInt();
-        billetes_de_20=billetes_de_20*20;
-        System.out.println(" Introduce un número de billetes de 5: ");
-        billetes_de_5=sc.nextInt();
-        billetes_de_5=billetes_de_5*5;
-        System.out.println(" Introduce un número de monedas de 1 euro: ");
-        monedas_euro=sc.nextInt();
-        euros=billetes_de_100+billetes_de_20+billetes_de_5+monedas_euro;
-        System.out.println("La cantidad introducida equivale a: " + euros + " euros");
-
-
-
-
+System.out.println("Tu coche tiene lo siguiente: " + renault.ruedas + " ruedas" + renault.puertas +
+        "puertas" + renault.motor + " cv");
 
 
 
