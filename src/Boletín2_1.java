be86d24117170a11cb59f9d3b17f26a303f16cdf
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-08 10:11:23 +0200

    añadiendo proframas

---
+++ src/Boletín2_1.java
@@ -0,0 +1,24 @@
+
+
+
+public class Boletín2_1 {
+
+   
+    public static void main(String[] args) {
+     
+        
+        
+        float base=6;  float altura=7;  float area;
+        
+        area=base*altura/2;
+        
+        System.out.println(" Area del triángulo: " +area);
+        
+        
+        
+        
+       
+        
+    }
+    
+}
---
+++ src/Boletín2_2.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+public class Boletín2_2 {
+
+   
+    public static void main(String[] args) {
+        
+        
+        float lado=7; float área;
+        
+        área=lado*lado;
+        área=(float)Math.pow(lado, 2);
+        //tambien
+        System.out.println(" El área del cuadrado es: " +área);
+        System.out.println("Area es: " + área);
+        //sin declarar la variable area
+        
+        
+    }
+    
+}
